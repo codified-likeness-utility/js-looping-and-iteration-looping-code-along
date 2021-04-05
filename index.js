8ca936aac2f2c52ef3866feb18841e634540e711
Ian Davies <mr.daviesian@gmail.com>
2021-04-05 17:28:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(stringNames, eventName) {
+    let newCards = []
+    for (let i =0; i < stringNames.length; i ++) {
+        newCards.push(`Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return newCards
+}
+
+function countDown() {
+   let countdown = 10
+   while (countdown >= 0) {
+       console.log(countdown--);
+   }
+}
+
